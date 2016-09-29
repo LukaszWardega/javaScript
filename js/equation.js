e48da10b72189839cbdc192bd6320303c30d0593
Łukasz Wardęga <ukasz Wardga>
2016-09-29 22:07:42 +0200

    dodane nowe zadanie

---
+++ js/equation.js
@@ -0,0 +1,20 @@
+//equation
+alert('test');
+
+
+var a = prompt('Podaj wartość a:'),
+    b = prompt('Podaj wartość b:'),
+    value = (a * a) + (2 * a* b) - (b * b);
+
+console.log(value);
+
+if  (value > 0)  {
+    console.log('Wynik wynosi: ' + value + ' więc jest dodatni');
+    alert('Wynik wynosi: ' + value + ' więc jest dodatni');
+}  else  if  (value < 0)  {
+    console.log('Wynik wynosi: ' + value + ' więc jest ujemny');
+    alert('Wynik wynosi: ' + value + ' więc jest ujemny');
+}  else  {
+    console.log('wynik równa się: ' + value );
+    alert('wynik równa się: ' + value );
+}
